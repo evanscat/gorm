eef67dc1b56cbde4e11b43bf46bce75668521e63
shenshuangquan <evanscat@163.com>
2020-01-09 11:36:27 +0800

    增加默认namer大小写控制

--- naming.go
+++ naming.go
@@ -15,6 +15,68 @@ type NamingStrategy struct {
 	Column Namer
 }
 
+func BuildDefaultNamer(uppercase bool) Namer{
+	return func(name string) string {
+		const (
+			lower = false
+			upper = true
+		)
+
+		if v := smap.Get(name); v != "" {
+			return v
+		}
+
+		if name == "" {
+			return ""
+		}
+
+		var (
+			value                                    = commonInitialismsReplacer.Replace(name)
+			buf                                      = bytes.NewBufferString("")
+			lastCase, currCase, nextCase, nextNumber bool
+		)
+
+		for i, v := range value[:len(value)-1] {
+			nextCase = bool(value[i+1] >= 'A' && value[i+1] <= 'Z')
+			nextNumber = bool(value[i+1] >= '0' && value[i+1] <= '9')
+
+			if i > 0 {
+				if currCase == upper {
+					if lastCase == upper && (nextCase == upper || nextNumber == upper) {
+						buf.WriteRune(v)
+					} else {
+						if value[i-1] != '_' && value[i+1] != '_' {
+							buf.WriteRune('_')
+						}
+						buf.WriteRune(v)
+					}
+				} else {
+					buf.WriteRune(v)
+					if i == len(value)-2 && (nextCase == upper && nextNumber == lower) {
+						buf.WriteRune('_')
+					}
+				}
+			} else {
+				currCase = upper
+				buf.WriteRune(v)
+			}
+			lastCase = currCase
+			currCase = nextCase
+		}
+
+		buf.WriteByte(value[len(value)-1])
+		var s string
+		if uppercase {
+			s = strings.ToUpper(buf.String())
+			smap.Set(name, s)
+		} else {
+			s = strings.ToLower(buf.String())
+			smap.Set(name, s)
+		}
+		return s
+	}
+}
+
 // TheNamingStrategy is being initialized with defaultNamingStrategy
 var TheNamingStrategy = &NamingStrategy{
 	DB:     defaultNamer,
